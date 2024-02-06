ff97d072d17af52a38b032f18a2932ce13cac36b
daniel-d7a <eyad.taym.saba@gmail.com>
2024-02-07 00:47:16 +0200

    optimized the transition animation

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -8,38 +8,7 @@ import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 import { usePathname } from "next/navigation";
 import { BsFillTriangleFill } from "react-icons/bs";
 export default function Template({ children }: { children: React.ReactNode }) {
-  const [scope, animate] = useAnimate();
-
   const pathname = usePathname();
-
-  const animation = {
-    initial: {
-      x: "-115%",
-    },
-    transition: {
-      ease: [0.83, 0, 0.17, 1],
-    },
-  };
-
-  useEffect(() => {
-    async function slide() {
-      if (!scope?.current) return;
-      await animate(
-        ".animation-parent > div",
-        { x: "0%" },
-        {
-          delay: stagger(0.05),
-          duration: 0.2,
-        }
-      );
-      await animate(
-        ".animation-parent > div",
-        { x: "115%" },
-        { delay: stagger(0.05, { startDelay: 0.3 }), duration: 0.2 }
-      );
-    }
-    slide();
-  }, [animate, scope]);
 
   const positions = [
     "top-0",
@@ -62,35 +31,37 @@ export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <ReactLenis root>
       <AnimatePresence>
-        <motion.main
-          ref={scope}
-          className="fixed top-0 left-0 h-[100dvh] w-[100vw] animation-parent z-50 pointer-events-none"
-        >
+        <main className="fixed flex top-0 h-[100dvh] w-[100vw] animation-parent z-50 pointer-events-none">
           {Array(8)
             .fill("")
             .map((_, i) => (
-              <motion.div
+              <div
                 key={i}
-                {...animation}
                 className={cn(
-                  "absolute w-[100vw] h-[25vh]",
+                  "absolute w-[100vw] h-[25dvh] bg-neutral-700 ",
                   i % 2 === 0
                     ? i % 4 !== 0
                       ? "bg-ocd-blue"
                       : "bg-ocd-yellow"
-                    : "bg-neutral-800",
+                    : "bg-neutral-700",
                   i % 2 !== 0 && "z-20",
                   positions[i]
                 )}
-              ></motion.div>
+                style={{
+                  transform: "translateX(-110vw)",
+                  animation:
+                    "slideTransition 0.6s cubic-bezier(0.16, 1, 0.3, 1) forwards",
+                  animationDelay: `${i * 0.05}s`,
+                }}
+              />
             ))}
-        </motion.main>
+        </main>
         <motion.div
           key={pathname}
           initial={{ opacity: 0, x: 0, y: 50 }}
           animate={{ opacity: 1, x: 0, y: 0 }}
           exit={{ opacity: 0, x: 0, y: 50 }}
-          transition={{ delay: 1.3 }}
+          transition={{ delay: 0.6 }}
         >
           <NavBar />
           {children}
