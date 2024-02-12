07ab2ead77578929526794b1b69890e1318e08f6
daniel-d7a <eyad.taym.saba@gmail.com>
2024-02-13 01:24:05 +0200

    updated form in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,6 +10,21 @@ import { usePathname } from "next/navigation";
 
 export const Footer = () => {
   const pathname = usePathname();
+
+  function sendEmail(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const data = new FormData(form);
+    const email = data.get("email") as string;
+    const message = data.get("message") as string;
+
+    const href = `mailto:ocdagencyy@gmail.com?subject=Let's work together!&from=${email}&body=${message}`;
+
+    if (typeof window !== "undefined") {
+      window.location.href = href;
+    }
+  }
+
   return (
     <footer className="text-white flex flex-col-reverse md:grid md:grid-cols-2 px-4 md:px-16 pb-10 mt-10 bg-neutral-900 pt-10">
       <p className="md:hidden block text-neutral-300 mt-10">
@@ -71,10 +86,13 @@ export const Footer = () => {
         </div>
       </div>
       {pathname !== "/Contact" && (
-        <div className="col-span-1 col-start-2 border-ocd-yellow border-4 rounded-2xl w-full p-4 pb-10">
+        <form
+          onSubmit={sendEmail}
+          className="col-span-1 col-start-2 border-ocd-yellow border-4 rounded-2xl w-full p-4 pb-10"
+        >
           <p className="text-xl">Hire Us</p>
           <p className="text-3xl font-semibold mt-6">
-            Lets Create a project together ?
+            Lets Create a project together `?
           </p>
           <div className="space-y-6">
             <div className="grid w-full items-center gap-1.5 mt-6">
@@ -84,7 +102,7 @@ export const Footer = () => {
               <Input
                 className="bg-neutral-900 rounded-md border-neutral-500 placeholder:text-neutral-300"
                 type="email"
-                id="email"
+                name="email"
                 placeholder="Email"
               />
             </div>
@@ -95,14 +113,14 @@ export const Footer = () => {
               <Textarea
                 className="bg-neutral-900 rounded-md border-neutral-500 placeholder:text-neutral-300"
                 placeholder="Type your message here."
-                id="message"
+                name="message"
               />
             </div>
             <ScrollButton className="font-semibold rounded-full w-full bg-ocd-blue text-xl py-4 border-4 border-ocd-blue">
               Let&apos;s Collaborate!
             </ScrollButton>
           </div>
-        </div>
+        </form>
       )}
       <p className="hidden md:block col-span-2 text-neutral-300">
         © 2024. All rights reserved
