fc53311cc05e6bfba19ab710cc1979ce3f7795cc
daniel-d7a <eyad.taym.saba@gmail.com>
2024-02-13 01:26:07 +0200

    added about page

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,7 +1,9 @@
-export default function AboutUs() {
+import { AboutUs } from "@/components/home/AboutUs";
+
+export default function AboutUsPage() {
   return (
-    <div className="min-h-screen grid place-items-center">
-      <p>about us</p>
+    <div className="min-h-screen grid place-items-center py-40">
+      <AboutUs />
     </div>
   );
 }
